1b73754f0e77c44129bd94c28d034f0c284543ec
asmmostafarafid <rafid365.info@example.com>
2021-02-11 01:59:50 +0600

    Finish Practice

---
+++ time.js
@@ -0,0 +1,27 @@
+function doSomething(){
+    console.log(5555);
+}
+console.log(22222);
+setTimeout(doSomething, 10000);
+console.log(33333);
+console.log(44444);
+console.log(66666666666);
+
+//
+
+setTimeout(() =>  {
+    console.log('See you Later ,')
+}, 70000)
+
+// Asynchronous Javascript setTimeOut,,,, Setinterval 
+
+
+/// serinterval 
+
+setInterval(function()  {
+    console.log('doing it again ')
+}, 1000);
+
+
+
+
